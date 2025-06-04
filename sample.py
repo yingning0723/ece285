0d5f8b613362e233e39b52b34df859ed6dd741ef
yingning0723 <yingningjia@gmail.com>
2025-06-04 01:27:13 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,8 +7,8 @@ def parse_arguments():
     """Returns parsed arguments"""
     parser = argparse.ArgumentParser(description="Sample images from diffusion model")
     parser.add_argument("checkpoint_name", type=str, default=None, help="Checkpoint name of diffusion model")
-    parser.add_argument("--n-samples", type=int, default=100, help="Number of samples to generate")
-    parser.add_argument("--n-images-per-row", type=int, default=10, help="Number of images each row contains in the grid")
+    parser.add_argument("--n-samples", type=int, default=400, help="Number of samples to generate")
+    parser.add_argument("--n-images-per-row", type=int, default=20, help="Number of images each row contains in the grid")
     parser.add_argument("--device", type=str, default=None, help="GPU device to use")
     parser.add_argument("--timesteps", type=int, default=None, help="Total timesteps for sampling")
     parser.add_argument("--beta1", type=float, default=None, help="Hyperparameter for DDPM")
